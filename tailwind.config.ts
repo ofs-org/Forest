442e7286bd784e78976ba9682a8839c44c0c2373
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-04-13 21:36:05 -0300

    chore:  initial structure

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: ".5rem",
+        sm: "1rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
