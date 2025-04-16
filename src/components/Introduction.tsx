6b2d5659d8acca352c9e3dd1bf5b1d94a6d98e4e
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-04-15 23:44:42 -0300

    chore:  Developed the responsiveness of the Introduction component

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -3,13 +3,13 @@ import wildbeast from "../assets/parceiros/wildbeast.svg";
 export function Introduction() {
   return (
     <section className="container">
-      <div className="bg-gradient-to-t container from-verde-950/80 overflow-hidden pb-8 pt-64 px-8 relative rounded-2xl text-white to-blue-800 z-0">
+      <div className="bg-gradient-to-t container from-verde-950/80 max-sm:pt-12 max-sm:px-4 overflow-hidden pb-8 pt-64 px-8 relative rounded-2xl text-white to-blue-800 z-0">
         <img
           className="-z-10 absolute inset-0 object-cover size-full"
           src={floresta}
           alt="Imagem de floresta"
         />
-        <div className="bg-verde-950 gap-8 inline-flex items-center mb-8 pl-4 pr-1 py-1 rounded-full">
+        <div className="bg-verde-950/60 flex flex-col gap-2 items-start mb-8 p-4 rounded-xl sm:bg-verde-950 sm:flex-row sm:gap-8 sm:inline-flex sm:items-center sm:pl-4 sm:pr-1 sm:py-1 sm:rounded-ful">
           Vagas para Dezembro abertas
           <a
             className="bg-verde-400 inline-flex px-4 py-2 rounded-full text-verde-900"
@@ -18,11 +18,11 @@ export function Introduction() {
             Reserve Hoje
           </a>
         </div>
-        <h1 className="font-serif max-w-screen-sm mb-20 text-5xl text-balance">
+        <h1 className="font-serif max-w-screen-sm mb-8 sm:mb-20 sm:text-5xl text-4xl text-balance">
           Venha Experimentar a Vida na Floresta
         </h1>
-        <div className="flex items-end justify-between">
-          <p className="text-sm">
+        <div className="items-end justify-between sm:flex">
+          <p className="max-sm:mb-4 text-sm">
             Melhores locais para visitar{" "}
             <a
               className="decoration-2 hover:no-underline underline underline-offset-4"
